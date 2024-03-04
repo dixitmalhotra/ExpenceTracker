9a8f60071d10be64872973266f78403f70b46f55
Dixit <ptc.Services122@gmail.com>
2024-03-04 12:36:07 +0530

    -- Build Category Model Page

---
+++ ExpenceTracker/ExpenceTracker/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenceTracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+    }
+}
